b79414fba92ea2b4ee49eab80600f9a50f560e70
mukhammadjon-s <Mukhammadjon_Sirojiddinov@epam.com>
2022-06-26 09:45:54 +0500

    Companies providers

--- src/modules/companies/companies.module.ts
+++ src/modules/companies/companies.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { companiesProviders } from './companies.providers';
 
-@Module({})
-export class CompaniesModule {}
+@Module({
+  imports: [DatabaseModule],
+  providers: [...companiesProviders]
+})
+export class CompaniesModule { }
