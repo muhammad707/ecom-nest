6da0099991600b4ff198f42289c0b242c70fc343
mukhammadjon-s <Mukhammadjon_Sirojiddinov@epam.com>
2022-06-29 14:46:05 +0500

    changes proto datatype

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { AppModule } from './app.module';
 import { NestFactory } from '@nestjs/core';
-import { INestMicroservice } from '@nestjs/common';
 
 import { grpcClientOptions } from './grpc-client.options';
 import { MicroserviceOptions } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.connectMicroservice(grpcClientOptions);
+  app.connectMicroservice<MicroserviceOptions>(grpcClientOptions);
   await app.startAllMicroservices();
 }
 bootstrap();
--- src/modules/company/dto/create-company.dto.ts
+++ src/modules/company/dto/create-company.dto.ts
@@ -32,4 +32,3 @@ export interface CreateCompanyResponse {
   error: string[];
   id: Types.ObjectId;
 }
-
