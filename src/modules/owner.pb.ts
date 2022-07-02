8e2d04a18513eac867493a26b68fc06ef3a569c1
mukhammadjon-s <Mukhammadjon_Sirojiddinov@epam.com>
2022-07-02 16:55:19 +0500

    updates owner.proto file

--- src/modules/owner.pb.ts
+++ src/modules/owner.pb.ts
@@ -1,11 +1,32 @@
-import { Observable } from "rxjs";
-import { GrpcMethod } from '@nestjs/microservices';
+/* eslint-disable */
+import { GrpcMethod, GrpcStreamMethod } from '@nestjs/microservices';
+import { Observable } from 'rxjs';
 
 export const protobufPackage = 'owner';
-export const COMPANY_PACKAGE_NAME = 'company';
-export const COMPANY_SERVICE_NAME = 'CompanyService';
 
-export interface CreateCompanyRequest {
+/** ENUMS */
+export enum Status {
+  ACTIVE = 0,
+  BLOCKED = 1,
+  PENDING = 2,
+  PAUSED = 3,
+  UNRECOGNIZED = -1,
+}
+
+export enum HolderType {
+  INDIVIDUAL = 0,
+  LTD = 1,
+  UNRECOGNIZED = -1,
+}
+
+export enum OrderType {
+  ALL = 0,
+  PREORDER = 1,
+  END_PRODUCT = 2,
+  UNRECOGNIZED = -1,
+}
+
+export interface CompanyData {
   status: Status;
   name: string;
   legalName: string;
@@ -23,36 +44,128 @@ export interface CreateCompanyRequest {
   email: string;
   orderType: OrderType;
   legalAddress: string;
-  address: Record<string, any>;
-  createdAt: Date;
-  updatedAt: Date;
-  deletedAt: Date;
+  address: Address | undefined;
 }
 
-export enum Status {
-  ACTIVE = 1,
-  BLOCKED = 2,
-  PENDING = 3,
-  PAUSED = 4,
+/** Address */
+export interface Address {
+  latitude: string;
+  longitude: string;
+  name: string;
+  street: string;
+  city: string;
+  home: string;
+  postalCode: string;
+  apartment: string;
+  comment: string;
+  domofon: string;
+  address: string;
+  regionId: number;
+  districtId: number;
 }
 
-export enum HolderType {
-  INDIVIDUAL = 1,
-  LTD = 2,
-}
-
-export enum OrderType {
-  ALL = 1,
-  PREORDER = 2,
-  END_PRODUCT = 3,
+export interface CreateCompanyRequest {
+  companyData: CompanyData | undefined;
 }
 
 export interface CreateCompanyResponse {
-  status: number;
+  id: string;
   error: string[];
+  status: number;
+}
+
+export interface GetCompaniesRequest {
+  skip: number;
+  limit: number;
+}
+
+export interface GetCompaniesResponse {
+  status: number;
+  count: number;
+  error: string[];
+  data: CompanyData[];
+}
+
+export interface GetCompanyRequest {
   id: string;
 }
 
-export interface ProductServiceClient {
-  createCompany(request: CreateCompanyRequest): Observable<CreateCompanyResponse>;
+export interface GetCompanyResponse {
+  status: number;
+  error: string[];
+  data: CompanyData | undefined;
 }
+
+export const OWNER_PACKAGE_NAME = 'owner';
+
+/** COMPANY SERVICE */
+
+export interface CompanyServiceClient {
+  createCompany(
+    request: CreateCompanyRequest,
+  ): Observable<CreateCompanyResponse>;
+
+  getCompany(request: GetCompanyRequest): Observable<GetCompanyResponse>;
+
+  getCompanies(request: GetCompaniesRequest): Observable<GetCompaniesResponse>;
+}
+
+/** COMPANY SERVICE */
+
+export interface CompanyServiceController {
+  createCompany(
+    request: CreateCompanyRequest,
+  ):
+    | Promise<CreateCompanyResponse>
+    | Observable<CreateCompanyResponse>
+    | CreateCompanyResponse;
+
+  getCompany(
+    request: GetCompanyRequest,
+  ):
+    | Promise<GetCompanyResponse>
+    | Observable<GetCompanyResponse>
+    | GetCompanyResponse;
+
+  getCompanies(
+    request: GetCompaniesRequest,
+  ):
+    | Promise<GetCompaniesResponse>
+    | Observable<GetCompaniesResponse>
+    | GetCompaniesResponse;
+}
+
+export function CompanyServiceControllerMethods() {
+  return function (constructor: Function) {
+    const grpcMethods: string[] = [
+      'createCompany',
+      'getCompany',
+      'getCompanies',
+    ];
+    for (const method of grpcMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(
+        constructor.prototype,
+        method,
+      );
+      GrpcMethod('CompanyService', method)(
+        constructor.prototype[method],
+        method,
+        descriptor,
+      );
+    }
+    const grpcStreamMethods: string[] = [];
+    for (const method of grpcStreamMethods) {
+      const descriptor: any = Reflect.getOwnPropertyDescriptor(
+        constructor.prototype,
+        method,
+      );
+      GrpcStreamMethod('CompanyService', method)(
+        constructor.prototype[method],
+        method,
+        descriptor,
+      );
+    }
+  };
+}
+
+export const COMPANY_SERVICE_NAME = 'CompanyService';
